866191ab9600bb4b8ba4659fc6ecae91dc1954d8
Zach Nusbaum <zach@zdn.dev>
2019-10-21 10:02:14 -0700

    show spinner when posting comment

--- src/components/applicant/new_comment.jsx
+++ src/components/applicant/new_comment.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Panel } from 'primereact/panel'
+import { ProgressSpinner } from 'primereact/progressspinner'
 import Axios from 'axios'
 
 const NewComment = (props) => {
   const [body, setBody] = React.useState('')
+  const [loading, setLoading] = React.useState(false)
   const { applicant } = props
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setLoading(true)
     Axios.post(`https://gateway.pincanna.com/api/v1/applicants/${applicant.locatorID}/comments`, {
       body: body,
     })
@@ -15,29 +18,36 @@ const NewComment = (props) => {
       .then((data) => {
         alert('Message sent.')
         setBody('')
+        setLoading(false)
       })
-      .catch((err) => alert('Your message was not sent.'))
+      .catch((err) => {
+        alert('Your message was not sent.')
+        setLoading(false)
+      })
   }
 
   return (
     <Panel header="Post a Comment" toggleable={true}>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="body">Send a Message</label>
-          <textarea
-            name="comment[body]"
-            id="body"
-            body
-            rows="4"
-            className="form-control"
-            onChange={(e) => setBody(e.target.value)}
-            value={body}
-          ></textarea>
-        </div>
-        <div className="form-group">
-          <input type="submit" value="Send Message" className="btn btn-outline-primary" />
-        </div>
-      </form>
+      {loading && <ProgressSpinner />}
+      {!loading && (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="body">Send a Message</label>
+            <textarea
+              name="comment[body]"
+              id="body"
+              body
+              rows="4"
+              className="form-control"
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Send Message" className="btn btn-outline-primary" />
+          </div>
+        </form>
+      )}
     </Panel>
   )
 }
