995136fef8f907b53c3fd891cea4d8263f0123c9
Zach Nusbaum <zach@zdn.dev>
2019-10-22 08:26:50 -0700

    add loading indicator when resume is uploading

--- src/components/applicant/upload.jsx
+++ src/components/applicant/upload.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { resumeUploaded } from '../../actions/applicant'
+import { ProgressSpinner } from 'primereact/progressspinner'
 
 const UploadResume = (props) => {
+  const [loading, setLoading] = useState(false)
   const fileField = React.createRef()
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    let file = fileField.current.files[0]
-    var formData = new FormData()
+
+    const formData = new FormData()
+    const file = fileField.current.files[0]
+    await setLoading(true)
     formData.append('applicant[certificate]', file)
     console.log(file)
     axios
@@ -17,33 +21,38 @@ const UploadResume = (props) => {
         },
       })
       .then((response) => response.data)
-      .then((data) => {
+      .then(async (data) => {
         if (data.updated == true) {
           alert('Resume uploaded!')
           resumeUploaded(data)
+          await setLoading(false)
         } else {
           alert('Error uploading resume.')
+          await setLoading(false)
         }
       })
       .catch((err) => console.log(err))
   }
   return (
     <div className="upload-resume">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="applicant-certificate" style={{ color: 'red', fontWeight: 800, textAlign: 'center' }}>
-            Please upload a resume to complete your application:
-          </label>
-          <input
-            type="file"
-            name="applicant[certificate]"
-            id="applicant-certificate"
-            ref={fileField}
-            className="form-control"
-          />
-        </div>
-        <input type="submit" value="Upload" className="btn btn-outline-primary" />
-      </form>
+      {loading && <ProgressSpinner />}
+      {!loading && (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="applicant-certificate" style={{ color: 'red', fontWeight: 800, textAlign: 'center' }}>
+              Please upload a resume to complete your application:
+            </label>
+            <input
+              type="file"
+              name="applicant[certificate]"
+              id="applicant-certificate"
+              ref={fileField}
+              className="form-control"
+            />
+          </div>
+          <input type="submit" value="Upload" className="btn btn-outline-primary" />
+        </form>
+      )}
     </div>
   )
 }
